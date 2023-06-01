a864d3946bd9d3548cfeed8033e5ecef76d0d330
igor <igortereschuk92@gmail.com>
2023-06-01 17:34:16 +0200

    migrated db

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "todo",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,16 @@ WSGI_APPLICATION = "todoapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "todo_service",
+            "passfile": os.path.join(BASE_DIR, ".todo_pgpass"),
+        },
+        #        "NAME": "tododb",
+        #        "USER": "todouser",
+        #        "PASSWORD": "todopass",
+        #        "HOST": "localhost",
+        #        "PORT": "5432",
     }
 }
 
