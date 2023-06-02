7664de118412ddd01e33db19992a0a86b25c8878
igor <igortereschuk92@gmail.com>
2023-06-02 14:12:18 +0200

    added FT scheme and test for create item

---
+++ functional_test.py
@@ -0,0 +1,30 @@
+import requests
+import unittest
+
+
+class TestTodoAPI(unittest.TestCase):
+    def setUp(self) -> None:
+        self.api_url = "http://localhost:8000/api/"
+
+    def test_create_todo(self) -> None:
+        data = {
+            "title": "Create backend for client todo app",
+            "complited": False,
+        }
+        response = requests.post(self.api_url + "todo/", json=data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()["title"], "Create backend for client todo app")
+        self.assertEqual(response.json()["complited"], False)
+
+    def test_get_todo(self) -> None:
+        pass
+
+    def test_update_todo(self) -> None:
+        pass
+
+    def test_delete_todo(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from todo import views
+
+
+urlpatterns = [
+    path("todo/", views.todo_view),
+]
